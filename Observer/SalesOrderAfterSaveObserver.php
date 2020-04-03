b6e0f295147a3ef46fbabeb5712e09f00499c446
mrnhi <nhihuule333@gmail.com>
2020-04-03 08:17:02 +0700

    update Observer/SalesOrderAfterSaveObserver.php

--- Observer/SalesOrderAfterSaveObserver.php
+++ Observer/SalesOrderAfterSaveObserver.php
@@ -2,6 +2,7 @@
 
 namespace Boolfly\GiaoHangNhanh\Observer;
 
+use Boolfly\GiaoHangNhanh\Model\Order\Processor;
 use Exception;
 use Magento\Customer\Model\AddressFactory;
 use Magento\Framework\Event\Observer;
@@ -14,31 +15,39 @@ class SalesOrderAfterSaveObserver implements ObserverInterface
     /**
      * @var QuoteRepository
      */
-    protected $quoteRepository;
+    private $quoteRepository;
 
     /**
      * @var AddressFactory
      */
-    protected $customerAddressFactory;
+    private $customerAddressFactory;
 
     /**
      * @var LoggerInterface
      */
-    protected $logger;
+    private $logger;
+
+    /**
+     * @var Processor
+     */
+    private $orderProcessor;
 
     /**
      * SalesOrderAfterSaveObserver constructor.
      * @param QuoteRepository $quoteRepository
      * @param AddressFactory $customerAddressFactory
      * @param LoggerInterface $logger
+     * @param Processor $orderProcessor
      */
     public function __construct(
         QuoteRepository $quoteRepository,
         AddressFactory $customerAddressFactory,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        Processor $orderProcessor
     ) {
         $this->logger = $logger;
         $this->quoteRepository = $quoteRepository;
+        $this->orderProcessor = $orderProcessor;
         $this->customerAddressFactory = $customerAddressFactory;
     }
 
@@ -50,14 +59,32 @@ class SalesOrderAfterSaveObserver implements ObserverInterface
         /** @var \Magento\Sales\Model\Order $order */
         $order = $observer->getEvent()->getOrder();
         $quote = $this->quoteRepository->getActive($order->getQuoteId());
-        $address = $this->customerAddressFactory->create()
-            ->load($quote->getShippingAddress()->getCustomerAddressId());
+        $order->setDistrict($quote->getDistrict());
+        $order->setShippingServiceId($quote->getShippingServiceId());
 
         try {
-            $address->setData('district', $quote->getDistrict());
-            $address->save();
+            $order->save();
+            $this->orderProcessor->syncOrder($order);
         } catch (Exception $e) {
-            $this->logger->error(__('Can\'t set district for customer address.'));
+            $this->logger->error(__(
+                'Can\'t send order with increment ID %1 to giaohangnhanh.',
+                $order->getIncrementId()
+            ));
+        }
+
+        $customerAddressId = $quote->getShippingAddress()->getCustomerAddressId();
+        $address = $this->customerAddressFactory->create()->load($customerAddressId);
+
+        if ($address->getId()) {
+            try {
+                $address->setData('district', $quote->getDistrict());
+                $address->save();
+            } catch (Exception $e) {
+                $this->logger->error(__(
+                    'Can\'t set district for customer address with ID %1.',
+                    $customerAddressId
+                ));
+            }
         }
     }
 }
