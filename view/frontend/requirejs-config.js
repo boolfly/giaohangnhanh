cfd28d83e91c6ce27f490594c9c6f76c16da9395
mrnhi <nhihuule333@gmail.com>
2020-04-07 19:32:37 +0700

    replace Magento_Checkout/js/model/shipping-rate-processor/new-address file to add district to extension_attribute.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     "map": {
         "*": {
-            'Magento_Checkout/js/model/shipping-save-processor/default': 'Boolfly_GiaoHangNhanh/js/model/shipping-save-processor/default'
+            'Magento_Checkout/js/model/shipping-save-processor/default': 'Boolfly_GiaoHangNhanh/js/model/shipping-save-processor/default',
+            'Magento_Checkout/js/model/shipping-rate-processor/new-address': 'Boolfly_GiaoHangNhanh/js/model/shipping-rate-processor/new-address'
         }
     }
 };
