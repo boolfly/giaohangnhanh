98f00253cabc81670ae466bfdba9c89193870a7d
mrnhi <nhihuule333@gmail.com>
2020-04-21 01:44:26 +0700

    add  Block/Customer/DataProviders/AdditionalConfig.php as an arguments of customer_address_edit block

---
+++ Block/Customer/DataProviders/AdditionalConfig.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Boolfly\GiaoHangNhanh\Block\Customer\DataProviders;
+
+use Boolfly\GiaoHangNhanh\Model\Config;
+use Magento\Framework\Serialize\SerializerInterface;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class AdditionalConfig implements ArgumentInterface
+{
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * Constructor
+     *
+     * @param Config $config
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(
+        Config $config,
+        SerializerInterface $serializer
+    ) {
+        $this->config = $config;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * Get districts
+     *
+     * @return array
+     */
+    public function getSerializedDistricts()
+    {
+        $districts = $this->config->getDistricts();
+        $data = [];
+        foreach ($districts as $district) {
+            $data[$district['region_id']][] = [
+                'districtName' => $district['district_name'],
+                'districtID' => $district['district_id']
+            ];
+        }
+        return $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJsonData(): string
+    {
+        return $this->serializer->serialize([
+            'districts' => $this->getSerializedDistricts()
+        ]);
+    }
+}
